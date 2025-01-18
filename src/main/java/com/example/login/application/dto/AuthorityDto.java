b87d54b3e93e6f5f96b5232ab2b0381d035e235e
mii2026 <mii2026@skku.edu>
2025-01-19 07:45:47 +0900

    [refactor #13] 변수 적은 DTO는 Builder 패턴 제거

--- src/main/java/com/example/login/application/dto/AuthorityDto.java
+++ src/main/java/com/example/login/application/dto/AuthorityDto.java
@@ -1,16 +1,11 @@
 package com.example.login.application.dto;
 
 import com.example.login.domain.user.enums.Authority;
-import lombok.AccessLevel;
-import lombok.Builder;
 
-@Builder(access = AccessLevel.PRIVATE)
 public record AuthorityDto(String authorityName) {
 
     public static AuthorityDto of(Authority authority){
-        return AuthorityDto.builder()
-                .authorityName(authority.name())
-                .build();
+        return new AuthorityDto(authority.name());
     }
 
 }
